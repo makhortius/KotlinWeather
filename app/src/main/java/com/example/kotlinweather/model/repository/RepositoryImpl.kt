142889065ac3fc1f8bf0e254bae8d9050cc105e6
makhortius <dmitriy_makhortov@mail.ru>
2021-10-26 23:42:10 +0300

    Домашняя работа по 7 уроку

--- app/src/main/java/com/example/kotlinweather/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/kotlinweather/model/repository/RepositoryImpl.kt
@@ -5,11 +5,25 @@ import com.example.kotlinweather.model.entities.City
 import com.example.kotlinweather.model.entities.Weather
 
 class RepositoryImpl : Repository {
-    override fun getWeatherFromServer(lat: Double, lng: Double): Weather {
-        val dto = WeatherLoader.loadWeather(lat, lng)
+    override fun getWeatherFromServer(lat: Double, lng: Double ): Weather {
+        val dto = WeatherRepo.api.getWeather(lat, lng).execute().body()
+        /*val dto = WeatherRepo.api.getWeather(lat, lng).enqueue(object : Callback<WeatherDTO> {
+            override fun onResponse(call: Call<WeatherDTO>, response: Response<WeatherDTO>) {
+                TODO("Not yet implemented")
+                if(response.isSuccessful) {
+                    ///
+                } else {
+                    //....
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherDTO>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })*/
         return Weather(
             temperature = dto?.fact?.temp ?: 0,
-            feelsLike = dto?.fact?.feels_like ?: 0,
+            feelsLike = dto?.fact?.feelsLike ?: 0,
             condition = dto?.fact?.condition
         )
     }
